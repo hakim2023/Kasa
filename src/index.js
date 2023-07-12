396eec65444b4b570efb29e451db8ec3032ffbef
hakim23 <hawkdown15@gmail.com>
2023-07-12 15:07:03 +0200

    first commit in building branch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './styles/index.css';
+// import Home from './pages/Home/';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Home />
+//   </React.StrictMode>
+// );
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/'
+import Logements from './pages/Logements/'
+import Header from './components/Header'
+ 
+ReactDOM.render(
+    <React.StrictMode>
+            <Header/>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/logements" element={<Logements />} />
+            </Routes>
+        
+        </Router>
+    </React.StrictMode>,
+document.getElementById('root')
+)
---
+++ src/pages/Logements/index.jsx
@@ -0,0 +1,11 @@
+
+function Logements() {
+    return (
+        <div>
+    
+            <h1>Logements</h1>
+        </div>
+    )
+}
+
+export default Logements
